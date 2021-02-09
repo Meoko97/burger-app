ca2d6c2b12c85b26b4e7179ce025c2ca7c3a4e58
Meoko97 <dgarmar97@gmail.com>
2021-02-09 13:08:18 +0100

    connect to firebase

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,40 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    UNSAFE_componentWillUpdate() {
+        console.log('[OrderSummary.js] WillUpdate');
+    }
+
+    render () {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map((igKey) => {
             return (
                 <li key={igKey}>
                     <span stye={{ textTransform: 'capitalize' }}>
                         {igKey}
                     </span>
-                    : {props.ingredients[igKey]}
+                    : {this.props.ingredients[igKey]}
                 </li>
             );
-        });
+        })
 
-    return(
-        <Fragment>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingrediets:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>
-                <strong>
-                    Total Price: {props.price.toFixed(2)}
-                </strong>
-            </p>
-            <p>
-                Continue to Checkout?
-            </p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Fragment>
-    );
+        return(
+            <Fragment>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingrediets:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>
+                    <strong>
+                        Total Price: {this.props.price.toFixed(2)}
+                    </strong>
+                </p>
+                <p>
+                    Continue to Checkout?
+                </p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Fragment>
+        );
+    }
+    
 };
 
-export default orderSummary;
+export default OrderSummary;
